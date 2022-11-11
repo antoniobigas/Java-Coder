f1dc438e7ebed1aa652b8573ea1b64a43065c686
Antonio Bigas <100382731+antoniobigas@users.noreply.github.com>
2022-11-10 22:25:32 -0300

    Adicionei novos arquivos

---
+++ Java-Cod3r/Curso-Java-Cod3r/Exercícios/Desafio 1 - Organização de projetos/fundamentos/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		// comentario 
+		// sentença de codigo sempre terminar com ;
+		
+		System.out.println("Hello world!");
+		
+		/* inicio linha
+		 * comentario de linha
+		 * linha1
+		 * linha2
+		 * abaixo fim linha
+		 */
+	}
+}
